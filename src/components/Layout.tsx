ddb7d06302a9998b52c8009858dbbcf4641ff01f
Dave Baskin <dfbaskin@gmail.com>
2020-11-23 11:22:31 -0500

    Updating about page.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,14 +3,17 @@ import { Footer } from "./Footer";
 import { Header } from "./Header";
 
 interface Props {
+  contentClassName?: string;
   children: ReactNode;
 }
 
-export function Layout({ children }: Props) {
+export function Layout({ contentClassName, children }: Props) {
+  const contentName =
+    "default-width" + (contentClassName ? ` ${contentClassName}` : "");
   return (
     <div className="page">
       <Header />
-      <div className="default-width">{children}</div>
+      <div className={contentName}>{children}</div>
       <Footer />
     </div>
   );
