0beefce25e00b8a1f674c3d99892644b1d90d02f
Dave Baskin <dfbaskin@gmail.com>
2023-10-10 16:39:00 -0400

    Fixing URL for images.

--- src/services/passThroughImageService.ts
+++ src/services/passThroughImageService.ts
@@ -5,6 +5,10 @@ import { baseService } from "astro/assets";
 // https://github.com/withastro/astro/issues/4109
 const service: LocalImageService = {
   ...baseService,
+  getURL({ src }) {
+    const path = typeof src === "string" ? src : src.src;
+    return path;
+  },
   async transform(buffer, transform) {
     return {
       data: buffer,
