e92b6d0a73f9fc0b44a3c8ab3045bb03203b30ca
Dave Baskin <dfbaskin@gmail.com>
2024-08-17 15:03:20 -0400

    Updating image configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
@@ -8,15 +8,7 @@ export default defineConfig({
   site: "https://dfbaskin.com",
   integrations: [mdx(), sitemap()],
   output: "server",
-  adapter: cloudflare(),
-  image: {
-    service: {
-      entrypoint: "./src/services/passThroughImageService",
-    },
-  },
-  // vite: {
-  //   build: {
-  //     minify: false,
-  //   },
-  // },
+  adapter: cloudflare({
+    imageService: 'passThroughImageService',
+  })
 });
