371c05b87bb09eac6ca4c90f805a8aa5456b8a8b
Dave Baskin <dfbaskin@gmail.com>
2024-08-17 16:29:11 -0400

    Updating blog post images.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,5 @@
 import { defineCollection, z } from "astro:content";
+import type { ImageMetadata } from "astro";
 
 const posts = defineCollection({
   // Type-check frontmatter using a schema
@@ -8,7 +9,7 @@ const posts = defineCollection({
     // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional().default("/posts-default-image.jpg"),
+    heroImage: z.custom<ImageMetadata>().optional(),
   }),
 });
 
